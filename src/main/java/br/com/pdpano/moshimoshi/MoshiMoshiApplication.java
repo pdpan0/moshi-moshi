08b334ae0de75038db8dfc6f3c4c70453f6e53cf
pdpan0 <lm.m4rtins@gmail.com>
2023-07-10 22:15:41 -0300

    feat: initial architecture

---
+++ src/main/java/br/com/pdpano/moshimoshi/MoshiMoshiApplication.java
@@ -0,0 +1,13 @@
+package br.com.pdpano.moshimoshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoshiMoshiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoshiMoshiApplication.class, args);
+	}
+
+}
